79adb7f088371db39de47058c698fa41f2311560
lv27693 <Chinthana.Perera@lv.com>
2014-11-12 16:58:21 +0000

    Chin Perera :  Changes to CSS

---
+++ app/php/upload-file.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: lv27693
+ * Date: 11/11/2014
+ * Time: 14:41
+ */
+
+//if they DID upload a file...
+if($_FILES['photo']['name'])
+{
+    //if no errors...
+    if(!$_FILES['photo']['error'])
+    {
+        //now is the time to modify the future file name and validate the file
+        $new_file_name = strtolower($_FILES['photo']['tmp_name']); //rename file
+        if($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
+        {
+            $valid_file = false;
+            $message = 'Oops!  Your file\'s size is to large.';
+        }
+
+        //if the file has passed the test
+        if($valid_file)
+        {
+            //move it to where we want it to be
+            move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/'.$new_file_name);
+            $message = 'Congratulations!  Your file was accepted.';
+        }
+    }
+    //if there is an error...
+    else
+    {
+        //set that to be the returned message
+        $message = 'Ooops!  Your upload triggered the following error:  '.$_FILES['photo']['error'];
+    }
+}
+
+
+/*$_FILES['field_name']['name']
+$_FILES['field_name']['size']
+$_FILES['field_name']['type']
+$_FILES['field_name']['tmp_name']*/
